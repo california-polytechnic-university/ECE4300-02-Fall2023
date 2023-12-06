9ef379ae215e75ec0cc40ab5a24ad45a67cfce8a
PJalv <134026493+PJalv@users.noreply.github.com>
2023-12-06 12:17:05 -0800

    Update charts.py

--- Group D/Code/charts.py
+++ Group D/Code/charts.py
@@ -3,14 +3,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def generate_bar_graph(file_path):
-    
     data = pd.read_csv(file_path)
 
+    
+    data['Execution Time (s)'] = pd.to_numeric(data['Execution Time (s)'], errors='coerce')
 
     grouped_data = data.groupby(['Script', 'Language'])['Execution Time (s)'].mean().unstack()
 
     num_iterations = data['Iteration'].max()
-
 
     print("Calculated Averages:")
     print(grouped_data)
@@ -27,6 +27,11 @@ def generate_bar_graph(file_path):
     for rect in ax.patches:
         width = rect.get_width()
         plt.text(width, rect.get_y() + rect.get_height() / 2, f'{width:.2f}', ha='left', va='center')
+
+   
+    for i, row in data.iterrows():
+        if pd.isnull(row['Execution Time (s)']):
+            ax.text(0, i, 'DNF', ha='left', va='center', color='red')
 
     plt.savefig('output_plot.jpg', format='jpg')
     plt.show()
